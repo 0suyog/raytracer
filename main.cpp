a1948fb6c4a4d8ad7853f2c9b370b36fdebbc359
0suyog <dahalsunischit@gmail.com>
2025-12-30 00:47:31 +0545

    progress indicator

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ostream>
 int main() {
   // defining image height and weight
   int image_height = 256;
@@ -6,6 +7,7 @@ int main() {
   // P3 header width height and max color
   std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
   for (int j = 0; j < image_height; j++) {
+    std::clog << "\rScanlines remaining:" << image_height - j << std::flush;
     for (int i = 0; i < image_width; i++) {
       auto r = double(i) / (image_width - 1);
       auto g = double(j) / (image_height - 1);
@@ -19,4 +21,5 @@ int main() {
     }
     std::cout << "\n";
   }
+  std::clog << "\rDone.                        \n" << std::flush;
 }
