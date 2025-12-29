f5de0c0faae564a845634ed9e5c36664c7e6463e
0suyog <dahalsunischit@gmail.com>
2025-12-30 00:17:26 +0545

    fixed off by one error

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ int main() {
   int image_width = 255;
   // P3 header width height and max color
   std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
-  for (int j = 0; j < image_height; j++) {
-    for (int i = 0; i < image_width; i++) {
+  for (int j = 0; j <= image_height; j++) {
+    for (int i = 0; i <= image_width; i++) {
       std::cout << i << " " << j << " " << (i + j) / 2 << " ";
     }
     std::cout << "\n";
